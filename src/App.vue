0bb9609b23c511c06bbdb400276bd98f8ae147d4
Kobra <kobra@Kobras-MBP.fritz.box>
2026-09-09 10:40:37 +0200

    add people overview layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,41 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <div class="app">
+    <header class="header">
+      <h1>Star Wars Explorer</h1>
+
+      <nav>
+        <button>People</button>
+        <button>Starships</button>
+        <button>⭐ Favorites</button>
+      </nav>
+    </header>
+
+    <main class="main">
+      <h2>People</h2>
+
+      <section class="filters">
+        <input
+            type="text"
+            placeholder="Search people..."
+        />
+
+        <select>
+          <option>All genders</option>
+          <option>Male</option>
+          <option>Female</option>
+          <option>Unknown</option>
+        </select>
+
+        <select>
+          <option>Name A → Z</option>
+          <option>Name Z → A</option>
+          <option>Height ↑</option>
+          <option>Height ↓</option>
+        </select>
+      </section>
+    </main>
+  </div>
 </template>
